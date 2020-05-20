ef72e1e5c088baf06a703c9ef364ac746594cee9
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-05-20 22:21:05 +0200

    stickler errors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //konyan,[0,1],0,X
 const playerController = (() => {
-  var Player = function (name, symbol) {
+  const Player = function (name, symbol) {
     this.name = name;
     this.moves = [];
     this.symbol = symbol;
@@ -25,9 +25,9 @@ const playerController = (() => {
   };
 })();
 
-var gameBoardController = (function () {
+const gameBoardController = (function () {
   //display the baord
-  var GameBoard = function (xPlayer, oPlayer) {
+  const GameBoard = function (xPlayer, oPlayer) {
     this.xPlayer = xPlayer;
     this.oPlayer = oPlayer;
     this.status = "Running";
@@ -36,16 +36,16 @@ var gameBoardController = (function () {
     this.playCount = 0;
   };
 
-  var boardWinner = function (game) {
+  const boardWinner = function (game) {
     const board = document.getElementsByClassName("box");
     document.getElementById("game-status").innerHTML = "status = running";
 
     for (let i = 0; i < board.length; i++) {
       if (game.playCount < 9) {
         board[i].addEventListener("click", function () {
-          if (board[i].innerHTML.trim() == "" && game.status == "Running") {
+          if (board[i].innerHTML.trim() === "" && game.status === "Running") {
             board[i].innerHTML = game.currentPlay.symbol;
-            if (game.currentPlay.symbol == "X") {
+            if (game.currentPlay.symbol === "X") {
               game.currentPlay = game.oPlayer;
             } else {
               game.currentPlay = game.xPlayer;
@@ -53,56 +53,56 @@ var gameBoardController = (function () {
             document.getElementById("player").innerHTML = game.currentPlay.name;
 
             if (
-              board[0].innerHTML == board[1].innerHTML &&
-              board[1].innerHTML == board[2].innerHTML &&
+              board[0].innerHTML === board[1].innerHTML &&
+              board[1].innerHTML === board[2].innerHTML &&
               board[0].innerHTML.trim() != ""
             ) {
               showWinner(game);
             } else if (
-              board[3].innerHTML == board[4].innerHTML &&
-              board[4].innerHTML == board[5].innerHTML &&
+              board[3].innerHTML === board[4].innerHTML &&
+              board[4].innerHTML === board[5].innerHTML &&
               board[3].innerHTML.trim() != ""
             ) {
               showWinner(game);
             } else if (
-              board[6].innerHTML == board[7].innerHTML &&
-              board[7].innerHTML == board[8].innerHTML &&
+              board[6].innerHTML === board[7].innerHTML &&
+              board[7].innerHTML === board[8].innerHTML &&
               board[6].innerHTML.trim() != ""
             ) {
               showWinner(game);
             } else if (
-              board[0].innerHTML == board[3].innerHTML &&
-              board[3].innerHTML == board[6].innerHTML &&
+              board[0].innerHTML === board[3].innerHTML &&
+              board[3].innerHTML === board[6].innerHTML &&
               board[0].innerHTML.trim() != ""
             ) {
               showWinner(game);
             } else if (
-              board[1].innerHTML == board[4].innerHTML &&
-              board[4].innerHTML == board[7].innerHTML &&
+              board[1].innerHTML === board[4].innerHTML &&
+              board[4].innerHTML === board[7].innerHTML &&
               board[1].innerHTML.trim() != ""
             ) {
               showWinner(game);
             } else if (
-              board[2].innerHTML == board[5].innerHTML &&
-              board[5].innerHTML == board[8].innerHTML &&
+              board[2].innerHTML === board[5].innerHTML &&
+              board[5].innerHTML === board[8].innerHTML &&
               board[2].innerHTML.trim() != ""
             ) {
               showWinner(game);
             } else if (
-              board[0].innerHTML == board[4].innerHTML &&
-              board[4].innerHTML == board[8].innerHTML &&
+              board[0].innerHTML === board[4].innerHTML &&
+              board[4].innerHTML === board[8].innerHTML &&
               board[0].innerHTML.trim() != ""
             ) {
               showWinner(game);
             } else if (
-              board[2].innerHTML == board[4].innerHTML &&
-              board[4].innerHTML == board[6].innerHTML &&
+              board[2].innerHTML === board[4].innerHTML &&
+              board[4].innerHTML === board[6].innerHTML &&
               board[2].innerHTML.trim() != ""
             ) {
               showWinner(game);
             } else {
               game.playCount++;
-              if (game.playCount == 9) {
+              if (game.playCount === 9) {
                 document.getElementById("reset").classList = "";
               }
             }
@@ -114,7 +114,7 @@ var gameBoardController = (function () {
     }
   };
 
-  var showWinner = function (game) {
+  const showWinner = function (game) {
     document.getElementById("winner").innerHTML = "with 3 straight symbols";
     document.getElementById("message").style.display = "block";
     document.getElementById("game-status").innerHTML = "status = game over";
@@ -134,15 +134,15 @@ var gameBoardController = (function () {
   };
 })();
 
-var globalController = (function (playerController, gameBoardController) {
+const globalController = (function (playerController, gameBoardController) {
   //controller that links other modules
 
-  var startGame = function (event) {
+  const startGame = function (event) {
     event.preventDefault();
     const player1Name = document.getElementById("player-1").value;
     const player2Name = document.getElementById("player-2").value;
-    var play1 = playerController.addPlayer(player1Name, "X");
-    var play2 = playerController.addPlayer(player2Name, "O");
+    const play1 = playerController.addPlayer(player1Name, "X");
+    const play2 = playerController.addPlayer(player2Name, "O");
 
     //SHOW TABLE
     document.querySelector(".table__header").classList = "table__header";
@@ -154,12 +154,12 @@ var globalController = (function (playerController, gameBoardController) {
     document.getElementById("player").innerHTML = play1.name;
     document.getElementById("player").value = play1.symbol;
 
-    var game = gameBoardController.newGame(play1, play2);
+    const game = gameBoardController.newGame(play1, play2);
     console.log("GAME", game);
     gameBoardController.showBoard(game);
   };
 
-  var setupEventListener = function () {
+  const setupEventListener = function () {
     const board = document.getElementsByClassName("box");
     document.getElementById("reset").addEventListener("click", function () {
       for (let i = 0; i < board.length; i++) {
