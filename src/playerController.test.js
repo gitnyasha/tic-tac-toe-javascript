c5efdc23cc2f0e5b746e0fecb05be932ba49816e
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-06 00:34:28 +0200

    fixed linter errors

--- src/playerController.test.js
+++ src/playerController.test.js
@@ -2,6 +2,5 @@ import playerController from './playerController';
 
 test('Player has a name', () => {
   const player = playerController.addPlayer('Marshall', 'O');
-  console.log('HE', player);
   expect(player.name).toBe('Marshall');
 });
