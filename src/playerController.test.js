7d83d00b9360d01a67dc0e76da47e6d9d8e86c7b
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-08 13:34:17 +0200

    add player moves test

--- src/playerController.test.js
+++ src/playerController.test.js
@@ -1,6 +1,11 @@
 import playerController from './playerController';
 
+const player = playerController.addPlayer('Marshall', 'O');
+
 test('Player has a name', () => {
-  const player = playerController.addPlayer('Marshall', 'O');
   expect(player.name).toBe('Marshall');
+});
+
+test('If player can make a move', () => {
+  expect(playerController.addPlayer(player.moves)).toBeTruthy();
 });
